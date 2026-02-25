bd4cffb95611fd7a1b2dae8369c2915f0d077891
KAUZZ <kaua_c_garcia@estudante.sesisenai.org.br>
2026-02-25 13:59:13 +0000

    UPDATE

--- atividade29.py
+++ atividade29.py
@@ -1,4 +1,3 @@
-Faça um algoritmo que receba o preço de um produto, calcule e mostre o novo preço, sabendo-se que este sofreu um desconto de 10%.
 preco = float(input("Digite o preço do produto: "))
 desconto = preco * 0.10
 novo_preco = preco - desconto
