0c344a546f636ec7a28bae18437efb692c14b37c
KAUZZ <kaua_c_garcia@estudante.sesisenai.org.br>
2026-02-25 14:18:55 +0000

    UPDATE

--- lista 1 python/atividade7.py
+++ lista 1 python/atividade7.py
@@ -1,2 +1,5 @@
-dias = float (input("insira um numero de dias : "))
+dia = int(input("Digite o dia: "))
+mes = int(input("Digite o mês: "))
+dias_passados = (mes - 1) * 30 + dia
+print("Desde o início do ano, se passaram", dias_passados, "dias.")
 
